10860f85768ed5fb9db2b0e90724aa4a593aa6fb
fatherofsevenchildren <kudienko030303@mail.ru>
2024-10-29 16:39:04 +0300

    fix style

--- buildGallows.sh
+++ buildGallows.sh
@@ -1,3 +1,2 @@
-rm src/main/java/senla/gallows/Application.class src/main/java/senla/gallows/game/*.class src/main/java/senla/gallows/printer/*.class
 javac src/main/java/senla/gallows/Application.java src/main/java/senla/gallows/game/*.java src/main/java/senla/gallows/printer/*.java
 java -cp src/main/java senla.gallows.Application
